d3c54b2456d5952169cbe9e6267affc59e80da5e
claraloforti <clara.loforti@gmail.com>
2026-03-06 21:13:33 +0100

    add function base per i file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
+
 import Homepage from "./pages/Homepage";
+import FilteredWhiskyPage from "./pages/FilteredWhiskyPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import WishlistPage from "./pages/WishlistPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
+
           <Route index element={<Homepage />} />
+
+          <Route path="/filtered" element={<FilteredWhiskyPage />} />
+
+          <Route path="/checkout" element={<CheckoutPage />} />
+
+          <Route path="/wishlist" element={<WishlistPage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
