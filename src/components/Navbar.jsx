d13c576ca01b03a997ab9a345af8c1d72ee82f97
claraloforti <clara.loforti@gmail.com>
2026-03-06 20:13:19 +0100

    navbar links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../clara.css";
+import { useState } from "react";
+
+function Navbar() {
+
+    const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (search.trim() !== "") {
+            navigate(`/filtered?search=${encodeURIComponent(search)}`);
+        }
+    };
+
+    return (
+        <div className="navbar">
+            <div className="container nav-links">
+                <Link to="/" className="home">HOME</Link>
+                {/* LOGO */}
+                <Link to="/" className="logo">
+                    <img
+                        src="/imgs/logo.png"
+                        alt="Heritage Whisky Reserve Logo"
+                        className="logo-img"
+                    />
+                </Link>
+                <div className="nav-icons">
+                    {/* SEARCH FORM */}
+                    <form className="search-form" onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Cerca..."
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
+                        <button type="submit">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                    <Link to="/wishlist">
+                        <i className="fa-solid fa-heart"></i>
+                    </Link>
+                    <Link to="/checkout">
+                        <i className="fa-solid fa-cart-shopping"></i>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
