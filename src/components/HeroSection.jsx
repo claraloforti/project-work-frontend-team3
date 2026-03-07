31b9d2c64c53d13c021f3b898f651a0a78a0ed3d
Valeria Gabrielli <gabvale53@gmail.com>
2026-03-07 17:29:59 +0100

    Aggiunta hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,11 +4,21 @@ function HeroSection() {
 
   return (
     <>
-    <div className='hero-card'>
-      <h1>Io sono la hero del tuo sito</h1>
-      <p>qui andranno inserite delle immagini a scorrimento o un video in loop</p>
+      <div className='hero-card'>
+        <video
+          src="../videos/fumo-whiskey.mp4"
+          autoPlay
+          muted
+          loop playsInline
+          className='hero-video'
+        />
+        <div className='hero-overlay'>
+          <h1>L'eredità del gusto, distillata.</h1>
+        <button className='hero-btn'>SCOPRI LA NOSTRA CANTINA</button>
+        </div>
+      </div>
 
-    </div>
+
     </>
   )
 }
