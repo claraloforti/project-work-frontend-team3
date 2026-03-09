0334f233c4496a52eab11bccb2b075a5c31100aa
Valeria Gabrielli <gabvale53@gmail.com>
2026-03-09 15:30:30 +0100

    feat. aggiunto onsale

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,22 +1,39 @@
 import HeroSection from "../components/HeroSection"
+import WhiskyCard from "../components/WhiskyCard"
+import whiskies from "../data/whiskies"
+
 import '../valeria.css'
+
 function Homepage() {
+
+  // filtro solo quelli in sconto
+  const onSaleWhiskies = whiskies.filter(
+    whisky => whisky.discount > 0
+  )
 
   return (
     <>
       <HeroSection />
+
       <div className="on-sale-section">
-        <h2 className="on-sale-title">I NOSTRI PRODOTTI IN PROMOZIONE</h2>
+        <h2 className="on-sale-title">
+          I NOSTRI PRODOTTI IN PROMOZIONE
+        </h2>
 
         <div className="cards-container">
-          <div className="whisky-card-mockup"></div>
-          <div className="whisky-card-mockup"></div>
-          <div className="whisky-card-mockup"></div>
-          <div className="whisky-card-mockup"></div>
+
+          {
+            onSaleWhiskies.map(whisky => (
+              <WhiskyCard
+                key={whisky.slug}
+                whisky={whisky}
+              />
+            ))
+          }
+
         </div>
       </div>
     </>
-
   )
 }
 
