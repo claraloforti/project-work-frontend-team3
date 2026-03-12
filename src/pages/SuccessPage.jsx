941ba4524c6a44d47d2a6b97d7e81816e635dd21
andrealangianese <andre.langia.01@gmail.com>
2026-03-12 16:10:30 +0100

    fix: aggiusto colore white alla page success

--- src/pages/SuccessPage.jsx
+++ src/pages/SuccessPage.jsx
@@ -6,8 +6,8 @@ function SuccessPage() {
     const orderId = searchParams.get("order_id");
 
     return (
-        <div style={{ textAlign: 'center', padding: '50px' }}>
-            <h1>Pagamento Completato! 🥃</h1>
+        <div className="whisky-info" style={{ textAlign: 'center', padding: '50px' , color: 'white'}}>
+            <h1 >Pagamento Completato! 🥃</h1>
             <p>Grazie per il tuo acquisto. Il tuo ordine è il <strong>#{orderId}</strong>.</p>
             <p>Controlla la tua email per il riepilogo (guarda anche nello spam!).</p>
             <Link to="/">Torna allo shop</Link>
